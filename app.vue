975395d3e02a5f5a3c69930cc087b619b085264f
kieuquan99 <kieuquan@townsoftvina.com>
2024-02-23 09:20:43 +0700

    update

--- app.vue
+++ app.vue
@@ -1,6 +1,31 @@
 <template>
   <div>
-    <h1>TowsoftVina</h1>
-    <NuxtWelcome />
+    <Head>
+      <Title>Towsoft Vina</Title>
+      <Meta name="description" content="Towsoft Vina" />
+      <Style type="text/css" children="body { background-color: green; }" ></Style>
+    </Head>
+
+    <h1>Towsoft Vina</h1>
   </div>
 </template>
+<script setup lang="ts">
+useHead({
+  title: 'Towsoft Vina',
+  meta: [
+    { name: 'description', content: 'Towsoft Vina' }
+  ],
+  bodyAttrs: {
+    class: 'Towsoft Vina'
+  },
+  script: [ { innerHTML: 'console.log(\'Hello world Towsoft Vina\')' } ]
+})
+useSeoMeta({
+  title: 'Towsoft Vina',
+  ogTitle: 'Towsoft Vina',
+  description: 'This is Towsoft Vina, let me tell you all about it.',
+  ogDescription: 'This is Towsoft Vina, let me tell you all about it.',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
