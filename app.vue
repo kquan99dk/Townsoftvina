318b1252c897bc7ef808883c7b6a08269fadf7d5
kieuquan99 <kieuquan@townsoftvina.com>
2024-02-23 09:42:41 +0700

    update

--- app.vue
+++ app.vue
@@ -10,6 +10,15 @@
     <h2>Townsoft Vina TownsoftVina</h2>
     <p>Townsoft Vina page</p>
     <h3>this is townsoftvina page</h3>
+    Giới thiệu công ty
+    Công Ty TNHH Townsoft Vina
+    Korea - Vietnam IT Outsourcing Frontier!
+
+    We are a startup established to provide Vietnamese IT technology and software development technology to Korean companies. We aim to grow into an IT software outsourcing company with more than 300 employees within the next 5 years
+
+    Web or Mobile Applications
+
+    We develop web or mobile applications requested by Korean companies. It is mainly software for accounting, payroll, healthcare, and apartment management business.
     <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TownsoftVina">
     <img src="https://lh3.googleusercontent.com/p/AF1QipMvs5vX0N7Stb_SY18pXeCmHMNT9PSr-HXAfd0i=s680-w680-h510" alt="TownsoftVina">
     <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TownsoftVina">
@@ -19,6 +28,16 @@
     <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TownsoftVina">
     <img src="https://lh3.googleusercontent.com/p/AF1QipNho1Wa1AVrx_74WFyST13jY0mvvF0_Vxjqz76n=s680-w680-h510" alt="TownsoftVina">
     <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TownsoftVina">
+
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
   </div>
 </template>
 <script setup lang="ts">
@@ -30,14 +49,20 @@ useHead({
   bodyAttrs: {
     class: 'Townsoft Vina'
   },
-  script: [ { innerHTML: 'console.log(\'Hello world Townsoft Vina\')' } ]
+  script: [ { innerHTML: 'console.log(\'Hello world Townsoft Vina\')' } ],
+  link: [{
+            hid: 'canonical',
+            rel: 'canonical',
+            href: 'https://townsoftvina-kquan99dk.vercel.app/'
+          }
+        ]
 })
 useSeoMeta({
   title: 'Townsoft Vina',
   ogTitle: 'Townsoft Vina',
   description: 'This is Townsoft Vina, let me tell you all about it.',
   ogDescription: 'This is Townsoft Vina, let me tell you all about it.',
-  ogImage: 'https://example.com/image.png',
+  ogImage: 'https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510',
   twitterCard: 'summary_large_image',
 })
 </script>
