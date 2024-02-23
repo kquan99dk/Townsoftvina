3e8fbc872e8501f4aebe094f3a1c4319cd0a0f0a
kieuquan99 <kieuquan@townsoftvina.com>
2024-02-23 09:31:41 +0700

    update

--- app.vue
+++ app.vue
@@ -1,41 +1,42 @@
 <template>
   <div>
     <Head>
-      <Title>Towsoft Vina</Title>
-      <Meta name="description" content="Towsoft Vina" />
+      <Title>Townsoft Vina</Title>
+      <Meta name="description" content="Townsoft Vina" />
       <Style type="text/css" children="body { background-color: green; }" ></Style>
     </Head>
 
-    <h1>Towsoft Vina</h1>
-    <h2>Towsoft Vina TowsoftVina</h2>
-    <p>Towsoft Vina page</p>
-    <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TowsoftVina">
-    <img src="https://lh3.googleusercontent.com/p/AF1QipMvs5vX0N7Stb_SY18pXeCmHMNT9PSr-HXAfd0i=s680-w680-h510" alt="TowsoftVina">
-    <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TowsoftVina">
-    <img src="https://lh3.googleusercontent.com/p/AF1QipNhhXJ6DONvVgJ9_rtR-Lr_qwshR25JqcIeTlLa=s680-w680-h510" alt="TowsoftVina">
-    <img src="https://lh3.googleusercontent.com/p/AF1QipNJ6OLtqcSOuTXnqhuO4E8FUk_uxYGrjJGzzUk9=s680-w680-h510" alt="TowsoftVina">
-    <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TowsoftVina">
-    <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TowsoftVina">
-    <img src="https://lh3.googleusercontent.com/p/AF1QipNho1Wa1AVrx_74WFyST13jY0mvvF0_Vxjqz76n=s680-w680-h510" alt="TowsoftVina">
-    <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TowsoftVina">
+    <h1>Townsoft Vina</h1>
+    <h2>Townsoft Vina TownsoftVina</h2>
+    <p>Townsoft Vina page</p>
+    <h3>this is townsoftvina page</h3>
+    <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TownsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipMvs5vX0N7Stb_SY18pXeCmHMNT9PSr-HXAfd0i=s680-w680-h510" alt="TownsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TownsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipNhhXJ6DONvVgJ9_rtR-Lr_qwshR25JqcIeTlLa=s680-w680-h510" alt="TownsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipNJ6OLtqcSOuTXnqhuO4E8FUk_uxYGrjJGzzUk9=s680-w680-h510" alt="TownsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TownsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TownsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipNho1Wa1AVrx_74WFyST13jY0mvvF0_Vxjqz76n=s680-w680-h510" alt="TownsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TownsoftVina">
   </div>
 </template>
 <script setup lang="ts">
 useHead({
-  title: 'Towsoft Vina',
+  title: 'Townsoft Vina',
   meta: [
-    { name: 'description', content: 'Towsoft Vina' }
+    { name: 'description', content: 'Townsoft Vina' }
   ],
   bodyAttrs: {
-    class: 'Towsoft Vina'
+    class: 'Townsoft Vina'
   },
-  script: [ { innerHTML: 'console.log(\'Hello world Towsoft Vina\')' } ]
+  script: [ { innerHTML: 'console.log(\'Hello world Townsoft Vina\')' } ]
 })
 useSeoMeta({
-  title: 'Towsoft Vina',
-  ogTitle: 'Towsoft Vina',
-  description: 'This is Towsoft Vina, let me tell you all about it.',
-  ogDescription: 'This is Towsoft Vina, let me tell you all about it.',
+  title: 'Townsoft Vina',
+  ogTitle: 'Townsoft Vina',
+  description: 'This is Townsoft Vina, let me tell you all about it.',
+  ogDescription: 'This is Townsoft Vina, let me tell you all about it.',
   ogImage: 'https://example.com/image.png',
   twitterCard: 'summary_large_image',
 })
