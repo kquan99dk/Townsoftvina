23097c8d0dff1d48cacd50f35944fa16b27eeddd
kieuquan99 <kieuquan@townsoftvina.com>
2024-02-23 09:25:04 +0700

    update

--- app.vue
+++ app.vue
@@ -7,6 +7,17 @@
     </Head>
 
     <h1>Towsoft Vina</h1>
+    <h2>Towsoft Vina TowsoftVina</h2>
+    <p>Towsoft Vina page</p>
+    <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TowsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipMvs5vX0N7Stb_SY18pXeCmHMNT9PSr-HXAfd0i=s680-w680-h510" alt="TowsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TowsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipNhhXJ6DONvVgJ9_rtR-Lr_qwshR25JqcIeTlLa=s680-w680-h510" alt="TowsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipNJ6OLtqcSOuTXnqhuO4E8FUk_uxYGrjJGzzUk9=s680-w680-h510" alt="TowsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TowsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TowsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipNho1Wa1AVrx_74WFyST13jY0mvvF0_Vxjqz76n=s680-w680-h510" alt="TowsoftVina">
+    <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TowsoftVina">
   </div>
 </template>
 <script setup lang="ts">
