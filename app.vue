7c1bb42755e3b1a2229fb2829673c87741af51a8
kieuquan99 <kieuquan@townsoftvina.com>
2024-02-23 09:50:54 +0700

    update

--- app.vue
+++ app.vue
@@ -6,12 +6,71 @@
       <Style type="text/css" children="body { background-color: green; }" ></Style>
     </Head>
 
-    <h1>Townsoft Vina</h1>
+    <h1>Townsoft Vina townsoftvina</h1>
     <h2>Townsoft Vina TownsoftVina</h2>
     <p>Townsoft Vina page</p>
     <h3>this is townsoftvina page</h3>
     Giới thiệu công ty
-    Công Ty TNHH Townsoft Vina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
+    Công Ty TNHH TownsoftVina townsoftvina
     Korea - Vietnam IT Outsourcing Frontier!
 
     We are a startup established to provide Vietnamese IT technology and software development technology to Korean companies. We aim to grow into an IT software outsourcing company with more than 300 employees within the next 5 years
@@ -29,6 +88,177 @@
     <img src="https://lh3.googleusercontent.com/p/AF1QipNho1Wa1AVrx_74WFyST13jY0mvvF0_Vxjqz76n=s680-w680-h510" alt="TownsoftVina">
     <img src="https://lh3.googleusercontent.com/p/AF1QipPRIx7v4he0GCfC4gVe4Ig-Om77a_W601FhqSLV=s680-w680-h510" alt="TownsoftVina">
 
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
+    Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
+    Tên viết tắt	TOWNSOFT VINA CO., LTD
+    Mã số thuế	0109151007
+    Địa chỉ	Tầng 6 Số 3 - A24 Ngõ 385 đường Hoàng Quốc Việt, Phường Nghĩa Tân, Quận Cầu Giấy, Thành phố Hà Nội, Việt Nam
+    Người đại diện	HAN A RAM
+    Ngày hoạt động	2020-04-06
+    Quản lý bởi	Cục Thuế Thành phố Hà Nội
+    Loại hình DN	Công ty trách nhiệm hữu hạn ngoài NN
+    Tình trạng	Đang hoạt động (đã được cấp GCN ĐKT)
     Tên quốc tế	TOWNSOFT VINA COMPANY LIMITED
     Tên viết tắt	TOWNSOFT VINA CO., LTD
     Mã số thuế	0109151007
